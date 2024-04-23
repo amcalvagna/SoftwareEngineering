6fd19209ad7414d155bee3730f5c9cfaf28eec76
Andrea Mario Calvagna <99475955+amcalvagna@users.noreply.github.com>
2024-04-23 11:46:22 +0200

    Add files via upload

--- innerApp/Test.java
+++ innerApp/Test.java
@@ -3,7 +3,7 @@ class Test {
 	public static void main(String args[]) { 
 		Context c = new Context(); 
 
-		//passo come inner class una istanza anonima sottoclasse di Strategy
+		//passo come inner class una istanza anonima, sottoclasse di Strategy
 		c.executeTask( new Strategy()  { 
 					       public String appliedStrategy(int x) { 
 						        return x+" squared"; 
