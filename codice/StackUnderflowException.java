e349da1bc9a028d7c1b42c1fa84a957361935aee
Andrea Mario Calvagna <99475955+amcalvagna@users.noreply.github.com>
2025-04-07 20:24:00 +0200

    Delete codice directory

--- codice/StackUnderflowException.java
+++
@@ -1,8 +0,0 @@
-public class StackUnderflowException extends Throwable{
-    private Stack<?> stack; 
-    StackUnderflowException(Stack<?> s) { this.stack=s;}
-    public String getMessage(){ 
-        return "an underflow exception has occurred in "+this.stack.getType();  
-    }
-    
-}
