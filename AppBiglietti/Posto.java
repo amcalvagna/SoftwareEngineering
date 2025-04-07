9c300977c963912d46438a9debd28630cd53c835
Andrea Mario Calvagna <99475955+amcalvagna@users.noreply.github.com>
2025-04-07 20:17:08 +0200

    Delete AppBiglietti directory

--- AppBiglietti/Posto.java
+++
@@ -1,8 +0,0 @@
-// Codice Java che implementa il design pattern Factory Method
-
-// Posto e' un Product
-public interface Posto {
-	public String getPosizione();
-	public int getCosto();
-	public String getSettore();
-}
