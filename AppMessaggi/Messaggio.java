4ce63e351949fd82d8d8171c98b4280fee25113d
Andrea Mario Calvagna <99475955+amcalvagna@users.noreply.github.com>
2025-04-07 20:18:16 +0200

    Delete AppMessaggi directory

--- AppMessaggi/Messaggio.java
+++
@@ -1,8 +0,0 @@
-// Codice Java che implementa il design pattern Decorator
-
-// Messaggio e' un Component
-public interface Messaggio {
-    public String getDestin();
-
-    public String getTesto();
-}
