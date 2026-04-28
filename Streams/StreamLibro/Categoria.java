7a526775ec2160d490ab8522d11f52059fd835eb
Andrea Mario Calvagna <99475955+amcalvagna@users.noreply.github.com>
2026-04-28 13:03:28 +0200

    Delete Streams directory

--- Streams/StreamLibro/Categoria.java
+++
@@ -1,7 +0,0 @@
-public enum Categoria {
-	FANTASY,
-	CYBERPUNK,
-	THRILLER,
-	STORICO,
-	INFORMATICA
-}
--- Streams/lambda/src/main/java/functionalinterfaces/Example01.java
+++
@@ -1,8 +0,0 @@
-package functionalinterfaces;
-
-import model.Student;
-
-@FunctionalInterface
-public interface Example01 {
-    String printStudentName(Student student);
-}
